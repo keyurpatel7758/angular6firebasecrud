5c147f44fce55b28dfd72175a2e2b0edf39a2aaf
keyur <keyur@idea2working.com>
2018-08-24 16:59:04 +0530

    - update the firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,5 @@
 export const environment = {
   production: false,
   firebaseconfig: {
-    apiKey: "AIzaSyDwzCfYb4R3mfXcNaEnSXGeIu70u_IzbLY",
-    authDomain: "angular5-49d2a.firebaseapp.com",
-    databaseURL: "https://angular5-49d2a.firebaseio.com",
-    projectId: "angular5-49d2a",
-    storageBucket: "angular5-49d2a.appspot.com",
-    messagingSenderId: "716973137933"
   }
 };
